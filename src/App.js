4a99783e43e74a5cadc44bf91b24c2a5f752bb5b
nasirdiu <nasir.joypurhat@gmail.com>
2022-08-20 14:18:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/Share/Nav/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
