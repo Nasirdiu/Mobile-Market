3d4696ef33741dffcb46201e1938789df8475aee
nasirdiu <nasir.joypurhat@gmail.com>
2022-08-20 14:53:16 +0600

    slider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import Blogs from "./components/Home/Blogs/Blogs";
+import Home from "./components/Home/Home/Home";
+
+import Footer from "./components/Share/Footer/Footer";
 import Nav from "./components/Share/Nav/Nav";
 
 function App() {
@@ -6,8 +10,10 @@ function App() {
     <div>
       <Nav></Nav>
       <Routes>
-        <Route></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Share/Nav/Nav.js
+++ src/components/Share/Nav/Nav.js
@@ -1,6 +1,29 @@
 import React from "react";
 import img from "../../../assets/icon/20273cfda041b47e89e057a4c2296928.png";
+import { Link } from "react-router-dom";
 const Nav = () => {
+    const menuItems = (
+        <>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/about">Contact Us</Link>
+          </li>
+          
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          
+          
+        </>
+)
   return (
     <div>
       <div class="navbar bg-base-200">
@@ -26,34 +49,7 @@ const Nav = () => {
               tabindex="0"
               class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  Parent
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menuItems}
             </ul>
           </div>
           <a class="btn btn-ghost normal-case text-xl">
@@ -62,40 +58,7 @@ const Nav = () => {
         </div>
         <div class="navbar-end hidden lg:flex">
           <ul class="menu menu-horizontal p-0">
-            <li>
-              <a>Home</a>
-            </li>
-            <li tabindex="0">
-              <a>
-                Phone List
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2">
-                <li>
-                  <a>Apple</a>
-                </li>
-                <li>
-                  <a>Sony</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Blog</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Contact</a>
-            </li>
+            {menuItems}
           </ul>
         </div>
         <div class="">
