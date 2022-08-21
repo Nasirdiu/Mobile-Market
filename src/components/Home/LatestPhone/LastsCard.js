2dcdf54241a9555a2fad3ef142d937f13e7c9417
nasirdiu <nasir.joypurhat@gmail.com>
2022-08-21 12:10:30 +0600

    lastphone

---
+++ src/components/Home/LatestPhone/LastsCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const LastsCard = ({ phone }) => {
+  console.log(phone);
+  return (
+    <div className="">
+      <div class="card w-96 bg-base-200 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img
+            src={phone.img}
+            alt=""
+            class="rounded-xl"
+          />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">{phone.name}</h2>
+          <p>Price :${phone.price}</p>
+          <div class="card-actions">
+            <button class="btn btn-success  btn-sm">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LastsCard;
--- src/components/Home/LatestPhone/LatestPhone.js
+++ src/components/Home/LatestPhone/LatestPhone.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import LastsCard from "./LastsCard";
 
 const LatestPhone = () => {
-    return (
-        <div>
-            <h1 className='text-3xl text-center font-bold uppercase
-            mt-5'>Latest <span className='text-purple-400'>Phone</span></h1>
-        </div>
-    );
+  const [last, setLast] = useState([]);
+  useEffect(() => {
+    fetch("lastphone.json")
+      .then((res) => res.json())
+      .then((data) =>setLast(data));
+  }, []);
+  return (
+    <div>
+      <h1
+        className="text-3xl text-center font-bold uppercase
+            mt-5"
+      >
+        Latest <span className="text-purple-400">Phone</span>
+      </h1>
+      {
+        last.map(phone=><div key={phone.id}>
+            <LastsCard phone={phone}></LastsCard>
+        </div>)
+      }
+    </div>
+  );
 };
 
 export default LatestPhone;
